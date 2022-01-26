890b1eb18efc7c6c6a27e01378ef38b83cc602b6
deptyped <deptyped@gmail.com>
2022-01-26 15:10:26 +0200

    Fix metrics collection

--- src/middlewares/collect-metrics.middleware.ts
+++ src/middlewares/collect-metrics.middleware.ts
@@ -2,13 +2,13 @@ import { NextFunction } from "grammy";
 import { Context } from "@bot/types";
 import { metricsService } from "@bot/services";
 
-export const middleware = () => (ctx: Context, next: NextFunction) => {
+export const middleware = () => async (ctx: Context, next: NextFunction) => {
   try {
     metricsService.updatesCounter.inc({
       from_id: ctx.from?.id,
       chat_id: ctx.chat?.id,
     });
-    return next();
+    return await next();
   } catch (e) {
     metricsService.updatesFailedCounter.inc({
       from_id: ctx.from?.id,
