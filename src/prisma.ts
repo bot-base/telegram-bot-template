d2b71db78dbe4b965741643d83d48c819df70d6a
deptyped <deptyped@gmail.com>
2022-04-15 11:40:13 +0300

    Add prisma logging

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,79 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { logger } from "@bot/logger";
 
-export const prisma = new PrismaClient();
+const parseParameters = (parameters: string): any[] => {
+  try {
+    return JSON.parse(parameters);
+  } catch {
+    return [];
+  }
+};
+
+export const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
+
+prisma.$on("query", (e: Prisma.QueryEvent) => {
+  const parameters = parseParameters(
+    e.params.replace(
+      /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.?\d* UTC/g,
+      (date) => `"${date}"`
+    )
+  );
+  const query = e.query.replace(
+    /(\?|\$\d+)/g,
+    (match, param, offset, string: string) => {
+      const parameter = JSON.stringify(parameters.shift());
+      const previousChar = string.charAt(offset - 1);
+
+      return (previousChar === "," ? " " : "") + parameter;
+    }
+  );
+
+  logger.debug({
+    msg: "database query",
+    query,
+    duration: e.duration,
+  });
+});
+
+prisma.$on("error", (e: Prisma.LogEvent) => {
+  logger.error({
+    msg: "database error",
+    target: e.target,
+    message: e.message,
+  });
+});
+
+prisma.$on("info", (e: Prisma.LogEvent) => {
+  logger.info({
+    msg: "database info",
+    target: e.target,
+    message: e.message,
+  });
+});
+
+prisma.$on("warn", (e: Prisma.LogEvent) => {
+  logger.warn({
+    msg: "database warning",
+    target: e.target,
+    message: e.message,
+  });
+});
