5431038f8626bbd3a9a0bf760537439cfbaaa8d0
deptyped <deptyped@gmail.com>
2022-05-25 23:56:45 +0300

    Fix local context middleware

--- src/middlewares/setup-local-context.middleware.ts
+++ src/middlewares/setup-local-context.middleware.ts
@@ -6,6 +6,6 @@ import { Context } from "@bot/types";
 export const middleware = () => (ctx: Context, next: NextFunction) => {
   return context.run({}, () => {
     ctx.local = context.getStore() as LocalContext;
-    next();
+    return next();
   });
 };
