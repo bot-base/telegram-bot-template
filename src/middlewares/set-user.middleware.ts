a90909cb05b594e3a4a7037c7daea1b140c94b64
deptyped <deptyped@gmail.com>
2022-06-08 23:18:34 +0300

    Remove unnecessary logging

--- src/middlewares/set-user.middleware.ts
+++ src/middlewares/set-user.middleware.ts
@@ -2,15 +2,9 @@ import { NextFunction } from "grammy";
 
 import { Context } from "@bot/types";
 import { usersService } from "@bot/services";
-import { logger } from "@bot/logger";
 
 export const middleware = () => async (ctx: Context, next: NextFunction) => {
   if (ctx.from?.is_bot === false) {
-    logger.info({
-      msg: "find a user",
-      user: ctx.from,
-    });
-
     const { id: telegramId, language_code: languageCode } = ctx.from;
 
     ctx.local.user = await usersService.upsertByTelegramId(telegramId, {
