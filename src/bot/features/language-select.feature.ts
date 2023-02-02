212ad9334aba137283408e238756f7c9aa6b9bd9
deptyped <deptyped@gmail.com>
2023-02-02 18:07:23 +0200

    Use callback-data for language change (#99)

--- src/bot/features/language-select.feature.ts
+++ src/bot/features/language-select.feature.ts
@@ -1,19 +1,40 @@
 import { Composer } from "grammy";
+import { changeLanguageData } from "~/bot/callback-data";
 import { logHandle } from "~/bot/helpers/logging";
-import { selectLanguageKeyboard } from "~/bot/keyboards";
+import { i18n } from "~/bot/i18n";
+import { createChangeLanguageKeyboard } from "~/bot/keyboards";
 import { Context } from "~/bot/types";
 
 const composer = new Composer<Context>();
 
 const feature = composer.chatType("private");
 
-feature.use(selectLanguageKeyboard);
-
 feature.command("language", logHandle("command-language"), async (ctx) => {
   await ctx.replyWithChatAction("typing");
   await ctx.reply(ctx.t("language.select"), {
-    reply_markup: selectLanguageKeyboard,
+    reply_markup: await createChangeLanguageKeyboard(ctx),
   });
+});
+
+feature.callbackQuery(changeLanguageData.filter(), async (ctx) => {
+  const { userService } = ctx.container.items;
+  const { code: languageCode } = changeLanguageData.unpack(
+    ctx.callbackQuery.data
+  );
+
+  if (i18n.locales.includes(languageCode)) {
+    ctx.scope.user = await userService.updateByTelegramId(ctx.from.id, {
+      data: {
+        languageCode,
+      },
+    });
+
+    await ctx.i18n.renegotiateLocale();
+
+    await ctx.editMessageText(ctx.t("language.changed"), {
+      reply_markup: await createChangeLanguageKeyboard(ctx),
+    });
+  }
 });
 
 export { composer as languageSelectFeature };
