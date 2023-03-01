fe8a24494ff715e952d78829db7e340da32a97e3
imMohika <mohi.mohammd222@gmail.com>
2023-03-02 02:51:36 +0330

    Use zod for config validation (#97)
    Co-authored-by: deptyped <deptyped@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,60 @@
 import "dotenv/config";
-import { cleanEnv, json, num, str } from "envalid";
-import { PollingOptions } from "grammy";
+import z from "zod";
 
-export const config = cleanEnv(process.env, {
-  NODE_ENV: str({ choices: ["development", "production"] }),
-  LOG_LEVEL: str({
-    choices: ["trace", "debug", "info", "warn", "error", "fatal", "silent"],
-  }),
-  DATABASE_URL: str(),
-  REDIS_URL: str(),
-  BOT_SERVER_HOST: str({
-    default: "0.0.0.0",
-  }),
-  BOT_SERVER_PORT: num({
-    default: 80,
-  }),
-  BOT_ALLOWED_UPDATES: json<PollingOptions["allowed_updates"]>({
-    default: [],
-  }),
-  BOT_TOKEN: str(),
-  BOT_WEBHOOK: str(),
-  BOT_ADMIN_USER_ID: num(),
+const updates = [
+  "message",
+  "poll",
+  "poll_answer",
+  "my_chat_member",
+  "chat_member",
+  "chat_join_request",
+  "edited_message",
+  "channel_post",
+  "edited_channel_post",
+  "inline_query",
+  "chosen_inline_result",
+  "callback_query",
+  "shipping_query",
+  "pre_checkout_query",
+] as const;
+
+const configSchema = z.object({
+  NODE_ENV: z.enum(["development", "production"]),
+  LOG_LEVEL: z.enum([
+    "trace",
+    "debug",
+    "info",
+    "warn",
+    "error",
+    "fatal",
+    "silent",
+  ]),
+  DATABASE_URL: z.string(),
+  REDIS_URL: z.string(),
+  BOT_SERVER_HOST: z.string().default("0.0.0.0"),
+  BOT_SERVER_PORT: z.coerce.number().positive().default(80),
+  BOT_ALLOWED_UPDATES: z.preprocess((v: unknown) => {
+    try {
+      return JSON.parse(String(v));
+    } catch (e) {
+      return null;
+    }
+  }, z.array(z.enum(updates))),
+  BOT_TOKEN: z.string(),
+  BOT_WEBHOOK: z.string().url(),
+  BOT_ADMIN_USER_ID: z.coerce.number().finite(),
 });
 
-export type Config = typeof config;
+const parseConfig = (env: NodeJS.ProcessEnv) => {
+  const config = configSchema.parse(env);
+
+  return {
+    ...config,
+    isDev: process.env.NODE_ENV === "development",
+    isProd: process.env.NODE_ENV === "production",
+  };
+};
+
+export type Config = ReturnType<typeof parseConfig>;
+
+export const config = parseConfig(process.env);
