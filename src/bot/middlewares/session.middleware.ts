f2ee3362e5d796bd6a4222d6477292114f61313d
abdoo9 <63903814+abdoo9@users.noreply.github.com>
2023-05-09 15:06:32 +0000

    Separate session storage for different bots (#131)
    Co-authored-by: deptyped <deptyped@gmail.com>

--- src/bot/middlewares/session.middleware.ts
+++ src/bot/middlewares/session.middleware.ts
@@ -1,10 +1,18 @@
 import { Middleware, StorageAdapter, session as createSession } from "grammy";
 import type { Context } from "~/bot/context";
 
+type ContextWithoutSession = Omit<Context, "session">;
 export const session = (
   storage: StorageAdapter<unknown>
 ): Middleware<Context> =>
   createSession({
     initial: () => ({}),
     storage,
+    getSessionKey(ctx: ContextWithoutSession) {
+      // Give every user their one personal session storage per bot
+      // (an independent session for the same user in each bot)
+      return ctx.chat === undefined || ctx.me === undefined
+        ? undefined
+        : `${ctx.chat.id}/${ctx.me.id}`;
+    },
   });
