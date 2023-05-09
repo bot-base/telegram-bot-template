4685996ab69495272344dfdb267df298f96cb2af
abdoo9 <63903814+abdoo9@users.noreply.github.com>
2023-05-09 04:19:04 +0300

    Update session.middleware.ts
    I have added a new feature to prevent session overlap for the same user in different bots. Although I acknowledge that the type definitions may not be perfect, I have done my best to implement them. This should help improve the overall user experience and prevent potential errors.

--- src/bot/middlewares/session.middleware.ts
+++ src/bot/middlewares/session.middleware.ts
@@ -1,10 +1,18 @@
 import { Middleware, StorageAdapter, session as createSession } from "grammy";
 import type { Context } from "~/bot/context";
 
+type ContextWithoutSession = Omit<Context, "session">;
 export const session = (
   storage: StorageAdapter<unknown>
 ): Middleware<Context> =>
   createSession({
     initial: () => ({}),
     storage,
+    getSessionKey(ctx: ContextWithoutSession) {
+      // Give every user their one personal session storage per bot
+      // (an independent session for the same user in each bot)
+      return ctx.from === undefined || ctx.me === undefined
+        ? undefined
+        : `${ctx.from.id}/${ctx.me.id}`;
+    },
   });
