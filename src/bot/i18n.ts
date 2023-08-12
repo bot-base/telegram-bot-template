5df6108f9d606e98d18824716cca5589fd698f7d
deptyped <deptyped@gmail.com>
2023-08-12 13:58:32 +0300

    Fix locales loading on serverless environments

--- src/bot/i18n.ts
+++ src/bot/i18n.ts
@@ -1,9 +1,10 @@
+import path from "node:path";
 import { I18n } from "@grammyjs/i18n";
 import type { Context } from "#root/bot/context.js";
 
 export const i18n = new I18n<Context>({
   defaultLocale: "en",
-  directory: "locales",
+  directory: path.resolve(process.cwd(), "locales"),
   useSession: true,
   fluentBundleOptions: {
     useIsolating: false,
